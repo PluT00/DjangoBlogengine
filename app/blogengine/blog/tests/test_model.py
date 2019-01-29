a1c73b5f8ae09f4728f4d9eb3cf18a461efd92fd
PluT00 <valeran2003@mail.ru>
2019-01-29 16:52:12 +0300

    Update test

--- app/blogengine/blog/tests/test_model.py
+++ app/blogengine/blog/tests/test_model.py
@@ -6,31 +6,31 @@ from blog.models import Post, Tag
 class PostModelTest(TestCase):
 
     def setUp(self):
-        Post.objects.create(title="NewPost", body="New post's body")
+        self.post = Post.objects.create(title="NewPost", body="New post's body")
 
     def test_get_absolute_url(self):
-        post = Post.objects.get(id=1)
+        post = self.post
         self.assertEqual(post.get_absolute_url(), '/blog/post/%s/' %post.slug)
 
     def test_get_update_url(self):
-        post = Post.objects.get(id=1)
+        post = self.post
         self.assertEqual(
             post.get_update_url(), '/blog/post/%s/edit' %post.slug
         )
 
     def test_get_delete_url(self):
-        post = Post.objects.get(id=1)
+        post = self.post
         self.assertEqual(
             post.get_delete_url(), '/blog/post/%s/delete/' %post.slug
         )
 
     def test_title_max_length(self):
-        post = Post.objects.get(id=1)
+        post = self.post
         max_length = post._meta.get_field('title').max_length
         self.assertEqual(max_length, 100)
 
     def test_slug_max_length(self):
-        post = Post.objects.get(id=1)
+        post = self.post
         max_length = post._meta.get_field('slug').max_length
         self.assertEqual(max_length, 150)
 
@@ -38,26 +38,26 @@ class PostModelTest(TestCase):
 class TagModelTest(TestCase):
 
     def setUp(self):
-        Tag.objects.create(title="NewTag")
+        self.tag = Tag.objects.create(title="NewTag")
 
     def test_get_absolute_url(self):
-        tag = Tag.objects.get(id=1)
+        tag = self.tag
         self.assertEqual(tag.get_absolute_url(), '/blog/tags/%s/' %tag.slug)
 
     def test_get_update_url(self):
-        tag = Tag.objects.get(id=1)
+        tag = self.tag
         self.assertEqual(tag.get_update_url(), '/blog/tags/%s/edit/' %tag.slug)
 
     def test_get_delete_url(self):
-        tag = Tag.objects.get(id=1)
+        tag = self.tag
         self.assertEqual(tag.get_delete_url(), '/blog/tags/%s/delete/' %tag.slug)
 
     def test_title_max_length(self):
-        tag = Tag.objects.get(id=1)
+        tag = self.tag
         max_length = tag._meta.get_field('title').max_length
         self.assertEqual(max_length, 50)
 
     def test_slug_max_length(self):
-        tag = Tag.objects.get(id=1)
+        tag = self.tag
         max_length = tag._meta.get_field('slug').max_length
         self.assertEqual(max_length, 100)
